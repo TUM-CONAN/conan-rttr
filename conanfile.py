b90058ec238740ad8e26565d8a4c933ec602c60c
Ulrich Eck <ulrich.eck@tum.de>
2023-04-25 22:57:28 +0200

    new conan 2 package: rttr

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,9 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, replace_in_file, collect_libs
 import os
-import shutil
-from conans import ConanFile, tools, CMake
+
 
 
 class rttrConan(ConanFile):
@@ -11,43 +14,63 @@ class rttrConan(ConanFile):
     description = """rttr project"""
     url = "https://github.com/ulricheck/conan-rttr"
 
-    generators = "cmake"
-
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "rtti": [True, False],
         }
 
-    default_options = (
-        "shared=True", 
-        "rtti=True", 
-    )
-
-    scm = {
-        "type": "git",
-        "subfolder": "rttr",
-        "url": "https://github.com/ulricheck/rttr.git",
-        "revision": "master"
+    default_options = {
+        "shared": True,
+        "rtti": True,
     }
 
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "master",
+            "url": "https://github.com/ulricheck/rttr.git"
+        }})
 
+    def source(self):
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder)
+        git.checkout(commit=sources["commit"])
 
-    def _cmake_configure(self):
-        cmake = CMake(self)
-        cmake.definitions["DBUILD_STATIC"] = not self.options.shared
-        cmake.definitions["BUILD_WITH_RTTI"] = self.options.rtti
-        cmake.configure(source_dir='rttr')
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
 
+        def add_cmake_option(option, value):
+            var_name = "{}".format(option).upper()
+            value_str = "{}".format(value)
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
+
+        for option, value in self.options.items():
+            add_cmake_option(option, value)
+
+        tc.cache_variables["BUILD_STATIC"] = not self.options.shared
+        tc.cache_variables["BUILD_WITH_RTTI"] = self.options.rtti
+        tc.cache_variables["BUILD_EXAMPLES"] = False
+        tc.cache_variables["BUILD_BENCHMARKS"] = False
+        tc.cache_variables["BUILD_UNIT_TESTS"] = False
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_folder")
 
     def build(self):
-        cmake = self._cmake_configure()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._cmake_configure()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
+        #self.cpp_info.libs = collect_libs(self)
         self.cpp_info.libs = ["rttr_core_d"] if self.settings.build_type == "Debug" else ["rttr_core"]
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,24 +1,37 @@
-from conans import ConanFile, CMake
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.build import can_run
 import os
 
 
-class TBBTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", "bin", "bin")
-        self.copy("*.so*", "bin", "lib")
-        self.copy("*.dylib", "bin", "lib")
-
     def test(self):
-        os.chdir("bin")
-        if self.settings.os != "Windows":
-            self.run("LD_LIBRARY_PATH=./ ./example")
-        else:
-            self.run(".%sexample" % os.sep)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
